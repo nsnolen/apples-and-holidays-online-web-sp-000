c8cca6b6643d69bdc695202ec852491018b0792f
Sara Nolen <nolensara1@gmail.com>
2021-01-10 17:12:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 holiday_hash.each do |season, holiday_name|
   if holiday_name == :Christmas
-    data.add {|supply| supply == "supply"}
+    data.add {|supply| supply == "Balloons"}
   end
 end
 end
