622691f5536b6e5ea59045584bcd1f565e33963f
Sara Nolen <nolensara1@gmail.com>
2021-01-10 17:57:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,8 +72,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.each do |season, holidays|
-    holidays.each do |holiday, supplies|
+holiday_hash.collect do |season, holidays|
+    holidays.collect do |holiday, supplies|
       holiday if supplies.include?("BBQ")
     end
   end
