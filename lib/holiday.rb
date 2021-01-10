1cc0dc0ad8a915f293b63eac82cbea6a80c65818
Sara Nolen <nolensara1@gmail.com>
2021-01-10 17:31:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,6 +50,7 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].each do |holiday, supplies|
     supplies << "Lights"
+  end
   holiday_hash[:winter].flatten
 
 end
