03f0f2e7d56243db60d278437dd84df2d74a46b3
Sara Nolen <nolensara1@gmail.com>
2021-01-10 17:42:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
   puts "#{attribute}: #{value}"
 end
 end
+end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
