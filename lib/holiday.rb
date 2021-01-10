ff6c7638650fc16a012f139be0261efac5a7a8ea
Sara Nolen <nolensara1@gmail.com>
2021-01-10 17:44:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    data.each do |attribute, value|
-  puts "#{attribute}: #{value}"
+    holidays.each do |holiday, supplies|
+    puts "#{holiday}: #{supplies}"
 end
 end
 end
