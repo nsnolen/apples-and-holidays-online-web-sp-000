7468524004a3a799dc0893b63aaafe077cdb173a
Sara Nolen <nolensara1@gmail.com>
2021-01-10 17:43:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.each do |holiday, data|
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
     data.each do |attribute, value|
   puts "#{attribute}: #{value}"
 end
