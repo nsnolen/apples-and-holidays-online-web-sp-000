70d5a6f4c52a46ec55482d945bf2619133009cd4
Sara Nolen <nolensara1@gmail.com>
2021-01-10 17:37:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash[:winter].each do |holiday, supplies|
+    if holiday != "Lights" then
+    supplies << "Lights"
+  end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
